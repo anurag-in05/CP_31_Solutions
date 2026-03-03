06ca6253300563cd859b65304ca2dffb1bd5eed9
anurag-in05 <anurag2005.iit@gmail.com>
2026-03-03 13:32:10 +0530

    Add/update D_Divide_and_Equalize.cpp

---
+++ 1300/D_Divide_and_Equalize.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define nl endl
+#define vl vector<ll>
+#define vi vector<int>
+#define vvl vector<vector<ll>>
+#define vpl vector<pair<ll,ll>>
+#define mp map<ll,ll>
+#define pr pair<ll,ll>
+#define pb push_back
+#define pop pop_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define ub upper_bound
+#define lb lower_bound
+#define ff first
+#define ss second
+#define yn(f) f ? cout<<"YES\n" : cout<<"NO\n"
+#define syn(f) f ? cout<<"Yes\n" : cout<<"No\n"
+
+using namespace std;
+
+void solve(){
+    int n;cin>>n;
+    map<int,int> m;
+    vl v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+        for(int j=2;j*j<=v[i];j++){
+            while(v[i]%j == 0){
+                v[i]/=j;
+                m[j]++;
+            }
+        }
+        if(v[i]>1) m[v[i]]++; // impt
+    }
+    for(auto it:m){
+        if(it.second%n != 0){
+            cout<<"NO"<<nl;
+            return;
+        }
+    }
+    cout<<"YES"<<nl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;t = 1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
