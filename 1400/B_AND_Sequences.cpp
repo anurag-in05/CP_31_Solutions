b07212b3b1ae2750ff58f15b69a888ba69515e4e
anurag-in05 <anurag2005.iit@gmail.com>
2026-03-17 17:46:47 +0530

    And_sequences

--- 1400/B_AND_Sequences.cpp
+++ 1400/B_AND_Sequences.cpp
@@ -3,7 +3,7 @@
 #define nl endl
 using namespace std;
 
-const int N = 20005;
+const int N = 2e5 + 5;
 ll fact[N];
 const int mod = 1e9+7;
 
@@ -13,16 +13,21 @@ void solve() {
     vector<int> v(n);for(auto &it:v)cin>>it,m[it]++;
     int alland = v[0];
     for(int i=1;i<n;i++) alland &= v[i];
-    
-    cout<<((m[alland]*(m[alland]-1))%mod)*fact[n-2]%mod<<endl;
+    ll cnt = m[alland];
+    if(cnt < 2){
+        cout<<0<<nl;
+        return;
+    }
+    ll ans = (cnt * (cnt - 1)) % mod;   
+    ans = (ans * fact[n - 2]) % mod;
+    cout << ans << nl;
 }
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     fact[0]=1;
-    for(int i=1;i<=N;i++) fact[i]=(1LL*fact[i-1]*i)%mod;
-
+    for(int i=1;i<N;i++) fact[i]=(1LL*fact[i-1]*i)%mod;
     ll t;t = 1;
     cin>>t;
     while(t--) solve();
